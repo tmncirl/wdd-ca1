4a753797954631af1eaca5b74870afe0f5ce0eb8
tmncirl <x24378831@student.ncirl.ie>
2024-12-08 19:40:16 +0000

    add html dom manipulation 2 for index page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
+const largeTextElem = document.querySelector('.ix-hero-text');
+const heroVideoElem = document.querySelector('.ix-hero-video');
+
+// Manipulation of DOM #2
+largeTextElem.addEventListener('click', () => {
+    if (largeTextElem.textContent === 'CLIMATE ACTION') {
+        // Already transformed
+        return;
+    }
+
+    largeTextElem.textContent = 'CLIMATE ACTION';
+    heroVideoElem.src = '/video/IX/climateaction.mp4';
+});
+
 const factRefreshButton = document.querySelector('.ix-refresh-fact-button');
 
 const factTitleElem = document.querySelector('.ix-fact-title');
@@ -13,14 +27,30 @@ const facts = [
         title: 'Food waste',
         body: 'The average Irish citizen throws out about 146kg of food waste per year, which is one of the highest in the EU.',
         source: 'https://www.epa.ie/our-services/monitoring--assessment/waste/national-waste-statistics/food/'
+    },
+    {
+        title: 'Economic cost',
+        body: 'By 2060, unchecked climate change will cost the global economy over $44 trillion.',
+        source: 'https://www.globalgiving.org/learn/climate-change-facts'
+    },
+    {
+        title: 'Underfunding',
+        body: 'Most local climate action groups are severely underfunded, and are therefore very limited in what they can do, so any amount of donation can help.',
+        source: 'https://www.globalgiving.org/learn/local-climate-change-solutions/'
     }
-]
+];
 
 factRefreshButton.addEventListener('click', () => giveRandomFact());
 
 // Manipulation of DOM #1
 function giveRandomFact() {
     const randomFact = facts[Math.floor(Math.random() * facts.length)];
+
+    // To make sure we do not show the same fact and make it look like it didnt do anything
+    if (factTitleElem.textContent == randomFact.title) {
+        giveRandomFact();
+        return;
+    }
 
     factTitleElem.textContent = randomFact.title;
     factBodyElem.textContent = randomFact.body;
